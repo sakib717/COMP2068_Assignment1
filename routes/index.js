fada071ce3dd71dab1c718eac3e35512225d356c
sakib717 <sakibsabbir7@gmail.com>
2016-02-10 06:46:42 -0500

    added pages and updated css

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
 module.exports = router;
