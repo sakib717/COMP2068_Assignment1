d92bdc440d9a919879e60ef523148a9413b0d412
sakib717 <sakibsabbir7@gmail.com>
2016-02-10 22:17:04 -0500

    Completed Assignment, added pictures, did some final touches before website goes live on azure

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on Azure' });
+  res.render('index', { title: 'SAKIB PATEL' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
 });
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'My Services' });
+});
+
 
 module.exports = router;
